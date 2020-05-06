5f7a7c991452cb44f2dd7b94b0286f0be9d348f4
CAM603 <cameronhawley@comcast.net>
2020-05-05 20:40:54 -0400

    working on classes

--- src/15_classes.py
+++ src/15_classes.py
@@ -19,11 +19,41 @@
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+# print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
 
 # Print it--also make this print more nicely
-print(geocache)
+# print(geocache)
+class Planet:
+
+    shape = 'round'
+
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
+    
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
+
+    @classmethod
+    def commons(cls):
+        return f'All planets are {cls.shape} because of gravity'
+    
+    @staticmethod
+    def spin(speed = '2000 miles per hour'):
+        return f'The planet spins and spins at {speed}'
+
+hoth = Planet('Hoth', 200000, 5.5, 'Hoth System')
+naboo = Planet('Naboo', 30000, 8, "Naboo System")
+
+print(f'Name is {hoth.name}')
+print(hoth.orbit())
+print(f'Name is {naboo.name}')
+print(naboo.orbit())
+print(hoth.commons())
+print(Planet.spin('a super fast speed'))
